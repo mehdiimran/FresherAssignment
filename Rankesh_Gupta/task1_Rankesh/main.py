a9361a496c0323ca32db51cd05faf5a6774159b3
rancho87 <rankeshgupta87@gmail.com>
2021-03-26 17:58:12 +0530

    task1 without pandas

--- Rankesh_Gupta/task1_Rankesh/main.py
+++ Rankesh_Gupta/task1_Rankesh/main.py
@@ -1,4 +1,3 @@
-import pandas as pd
 from lxml import etree
 import logging
 import os
@@ -7,25 +6,40 @@ os.chdir('C://Users//guptaran//Desktop//Python')
 
 log_format = '%(asctime)s : %(message)s'
 logging.basicConfig(filename="test.log", level=logging.DEBUG, format=log_format)
-logging.debug("Reading Record of Csv file successfully done")
-data_file = pd.read_csv("sample_input.csv", usecols=[0, 1, 2])
 result = []
+unique_result = []
 
 
-def unique_record_of_auditSubscriber():
-    logging.debug("Searching Unique Record of AuditSubscriber Successfully done")
-    unique_words = data_file['MSISDN'].unique()
-    for row in data_file.values:
-        for unique_field in unique_words:
-            if unique_field in row:
-                result.append(row.tolist())
+def read_csv_file():
+    logging.debug("Reading Record of Csv file successfully done")
+    with open('sample_input.csv', 'r') as f:
+        for line in f:
+            words = line.split(',')
+            result.append(words[0:3])
 
+    print "\nPrinting Records of Csv .......\n"
     for i in result:
         print i
 
 
+def unique_record_of_auditSubscriber():
+    logging.debug("finding Unique Record of AuditSubscriber Successfully done")
+    for row in result:
+        MSISDN = row[0]
+        flag = True
+        for unique_row in unique_result:
+            if MSISDN == unique_row[0]:
+                flag = False
+                break
+        if flag:
+            unique_result.append(row[:3])
+    print "\nPrinting Unique Record Based on MSISDN of Csv .......\n"
+    for i in unique_result:
+        print i
+
+
 def create_xml_for_single_auditSubscriber():
-    logging.debug("Single Record Xml File Created for AuditSubscriber Successfully done" )
+    logging.debug("Single Record Xml File Created for AuditSubscriber Successfully done")
     xsi = "http://www.w3.org/2001/XMLSchema-instance"
     schemaLocation = "schema.xsd"
     root_config = etree.Element("Audit",
@@ -35,11 +49,11 @@ def create_xml_for_single_auditSubscriber():
     root_config = etree.SubElement(root_config, "Audit")
     auditSubscribers = etree.SubElement(root_config, "auditSubscribers")
     MSISDN = etree.SubElement(auditSubscribers, "MSISDN")
-    MSISDN.text = str(data_file['MSISDN'][0])
+    MSISDN.text = str(result[0][0])
     OperationType = etree.SubElement(auditSubscribers, "OperationType")
-    OperationType.text = str(data_file['OperationType'][1])
+    OperationType.text = str(result[0][1])
     ServiceIndication = etree.SubElement(auditSubscribers, "ServiceIndication")
-    ServiceIndication.text = str(data_file['ServiceIndication'][2])
+    ServiceIndication.text = str(result[0][2])
     tree = etree.ElementTree(root_config)
     tree.write("singleAuditSubscriber.xml", encoding='utf-8', xml_declaration=True, pretty_print=True)
 
@@ -67,18 +81,18 @@ def create_xml_for_mutiple_auditSubscriber():
 
 def configuration_service_indication():
     logging.debug("configuration file filter on basis of user Service Indication Successfully done")
-    input_string = raw_input("Enter service indication to filter")
-    data_file1 = pd.read_csv("sample_input.csv")
+    input_string = raw_input("\nEnter a service (VM_SUBPROFILE / SMS_SUBPROFILE) to filter  \n")
     result1 = []
-    for row1 in data_file1.values:
+    for row1 in result:
         if input_string in row1:
-            result1.append(row1.tolist())
+            result1.append(row1)
 
     for i in result1:
         print i
 
 
 if __name__ == "__main__":
+    read_csv_file()
     unique_record_of_auditSubscriber()
     create_xml_for_single_auditSubscriber()
     create_xml_for_mutiple_auditSubscriber()
